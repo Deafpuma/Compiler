7f7acfe40adfa25bb14e1bd573e28f7243910cf0
Deafpuma <47797733+Deafpuma@users.noreply.github.com>
2019-02-19 17:05:11 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01
+ {
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        { 
+
+            if (args.Length == 0)
+            {
+                Console.Write("Please Input a filename.");
+            }
+            else
+            {
+                string filename = args[0];
+                string[] fileLines = System.IO.File.ReadAllLines(filename);
+                var map = new Dictionary<string, string>();
+                foreach (string mLine in fileLines)
+                {
+                    if (mLine.Length >= 1)
+                    {
+                        string[] sides = mLine.Split(new string[] { "->" }, 2, System.StringSplitOptions.None);
+                        sides[0].Trim();
+                        sides[1].Trim();
+
+                        if (map.ContainsKey(sides[0]))
+                        {
+                            Console.Write("Left hand side repeated!");
+                            break;
+                        }
+                        else
+                        {
+                            try
+                            {
+                                var rex = new Regex(sides[1]);
+                                map.Add(sides[0], sides[1]);
+                                //Console.Write("Added:" +sides[1]+ " to the dictionary\n");
+                            }
+                            catch (Exception e)
+                            {
+                                Console.Write(e);
+                                continue;
+                            }
+
+                        }
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.Write("Completed!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
